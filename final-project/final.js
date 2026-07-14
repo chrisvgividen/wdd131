4f6d2b568103c5aebdb9e2e60177b7dc0014425c
chrisvgividen <c.v.givich@gmail.com>
2026-07-13 20:37:49 -0600

    Started progress on my final project, and made good progress. Created the finals folder, html, css, and js. Added a pictures folder as well

---
+++ final-project/final.js
@@ -0,0 +1,26 @@
+const repairs = [
+  {
+    name: "",
+    tags: ["", "", ""],
+    imgSrc:
+      "img pathing here",
+    imgAlt: "Image description here",
+    description:
+      "",
+    price:
+      "",
+      difficulty: 1
+  },
+  {
+    name: "",
+    tags: ["", "", ""],
+    imgSrc:
+      "img pathing here",
+    imgAlt: "Image description here",
+    description:
+      "",
+    price:
+      "",
+      difficulty: 1
+  }
+];
